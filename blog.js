b81154d2ca72f6e145ecc7fa91fb0bcc5da152d1
Enesaslan63 <enes.aslan0506@gmail.com>
2024-12-19 11:11:19 +0300

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,4 @@
+function hakkımda() {
+    const a = "Merhabalar ben Muhammed Enes Aslan. Harran Üniversitesinde Bilgisayar Mühendisliği 3.sınıf öğrencisiyim. ";
+    alert(a); // "Enes Ben" yazan bir uyarı gösterir
+}
